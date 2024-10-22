6323226e6e9ec5c48718c0cd120b18167fa6ec21
smbauer <steve_bauer61@hotmail.com>
2024-10-22 00:09:01 -0400

    refactored with error handling

--- Day26/main.py
+++ Day26/main.py
@@ -9,8 +9,16 @@ df = pd.read_csv("Day26/nato_phonetic_alphabet.csv")
 # convert to dictionary without columns headers/index
 alpha_dict = {row.letter:row.code for (index, row) in df.iterrows()}
 
-# create list of phonetic code words from a word the user inputs
-user_word = input("Enter a word: ").upper()
-code_words = [alpha_dict[letter] for letter in user_word]
+is_valid = False
 
+# create list of phonetic code words from a word the user inputs
+while not is_valid:
+    user_word = input("Enter a word: ").upper()
+    try:
+        code_words = [alpha_dict[letter] for letter in user_word]
+    except KeyError:
+        print("Only letters in the alphabet are allowed")
+    else:
+        is_valid = True
+        
 print(code_words)
