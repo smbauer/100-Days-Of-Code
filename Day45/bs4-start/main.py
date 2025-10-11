a700da2c23399254c3ef70b989232e61cc07d146
smbauer <steve_bauer61@hotmail.com>
2025-10-10 23:26:48 -0400

    refactored and added comments

--- Day45/bs4-start/main.py
+++ Day45/bs4-start/main.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 
 url = "https://news.ycombinator.com/"
 
+# get html text from url
 response = requests.get(url)
 yc_web_page = response.text
 
@@ -10,16 +11,17 @@ soup = BeautifulSoup(yc_web_page, "html.parser")
 
 titles = []
 links = []
-scores = []
 
+# store the article headlines and links in lists
 for titleline in soup.find_all(name="span", class_="titleline"):
     if titleline.a:
         titles.append(titleline.a.getText())
         links.append(titleline.a.get("href"))
 
-for score in soup.find_all(name="span", class_="score"):
-    scores.append(int(score.getText().split()[0]))
+# get the total points/upvotes from each article
+scores = [int(score.getText().split()[0]) for score in soup.find_all(name="span", class_="score")]
 
+# scores and headlines are not in the same selector, find the index of the article with the highest score
 max_score = max(scores)
 max_index = scores.index(max_score)
 
